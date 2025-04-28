9d4940e716c25ad770a152737258b83e10fad696
Jonas <jonas-august@hotmail.com>
2025-04-28 20:11:03 -0300

    Adicionar o Desafio

---
+++ Desafio.py
@@ -0,0 +1,16 @@
+nome = input("Digite o seu nome: ")
+
+salario = float(input("Digite o seu Salario: "))
+
+bonus = float(input("Digite o bonus: "))
+
+total_salario = (salario)
+total_bonus = (bonus)
+valor = 1000
+
+valor_final =  valor + total_salario * total_bonus
+
+print(f"O usuario {nome} possui o bonus de {valor_final}")
+
+
+
