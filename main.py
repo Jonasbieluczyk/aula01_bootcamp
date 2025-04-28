4e8c941749ffbeef0be10ef0adc026ff6a8a5dd3
Jonas <jonas-august@hotmail.com>
2025-04-28 20:12:37 -0300

    Adicionando melhorias

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-print("Hello world!")
+#crie um programa que o usuario digita o seu nome e retorna a quantidade de caracteres
+#print(len(input("Digite o seu nome: ")))
+
+# crie um programa aonde digita dois e valores e faça a soma deles
+
+print(int(input("Digite o primeiro numero: ")) + int(input("Digite o segundo numero:")))
+
